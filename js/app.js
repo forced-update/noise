3cc179ee115672cea6eae445bf0cb98f79910e61
Forced Update <>
2020-03-06 10:19:07 -0600

    looping

--- js/app.js
+++ js/app.js
@@ -1,38 +1,20 @@
-// WebAudio
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-
-const audioContext = new AudioContext();
-
-// get the audio element
-const audioElement = document.querySelector('audio#camping-sound-source');
-
-// pass it into the audio context
-const track = audioContext.createMediaElementSource(audioElement);
-
-track.connect(audioContext.destination);
-
 // select our play button
 const playButton = document.querySelector('button#camping-sound-control');
 
+var sound = new Howl({
+  src: ["audio/campfire_noise.ogg"],
+  loop: true,
+  volume: 0.9
+});
+
 playButton.addEventListener('click', function() {
-
-    // check if context is in suspended state (autoplay policy)
-    if (audioContext.state === 'suspended') {
-        audioContext.resume();
-    }
-
     // play or pause track depending on state
     if (this.dataset.playing === 'false') {
-        audioElement.play();
+        sound.play();
         this.dataset.playing = 'true';
     } else if (this.dataset.playing === 'true') {
-        audioElement.pause();
+        sound.pause();
         this.dataset.playing = 'false';
     }
 
-}, false);
-
-audioElement.addEventListener('ended', () => {
- //  audioElement.play();
-  playButton.dataset.playing = 'false';
 }, false);
